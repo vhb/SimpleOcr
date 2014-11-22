a3f97ae67672b37e2b148b4bb2a1d3c163f82f4f
Jeremy Quillere <mideuger@gmail.com>
2014-11-22 16:39:25 +0100

    add test script

---
+++ test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import subprocess
+
+argc = len(sys.argv)
+if argc < 4:
+	print 'usage: ./tesy.py workflow path_to_data dataset training_datas'
+	exit()
+
+workflow_path = str(sys.argv[1])
+dataset_path = str(sys.argv[2])
+training_path = str(sys.argv[3])
+
+workflow_file = open(dataset_path)
+workflow = json.load(workflow_file)
+
+total = float(len(workflow))
+errors = float(0)
+
+workflow = sorted(workflow.items())
+
+for key, value in workflow:
+	cmd = './ocr ' + value + ' ' + workflow_path + ' ' + dataset_path + ' ' + training_path + ' | grep \'output value:\' | sed \'s/output value: //g\''
+	result = subprocess.check_output(['sh', '-c', cmd])[:-1]
+	print key + ", " + result
+	if key != result:
+		errors += 1
+
+workflow_file.close()
+
+print (total - errors) / (total) * 100
