7aedb339fc829ccbb28c35f022bd624391cd1f3f
Jeremy Quillere <mideuger@gmail.com>
2014-11-26 23:28:32 +0100

    add dataset2

--- data/rename.py
+++ data/rename.py
@@ -4,9 +4,9 @@ for i in sys.argv[1:][::-1]:
     tmp = i
     i = i[0]
     if i.islower():
-        dest = i[0] + "_small" + ".bmp"
+        dest = i[0] + "_small" + ".jpg"
     elif i.isdigit():
-        dest = "num_" + i + ".bmp"
+        dest = "num_" + i + ".jpg"
     else:
-        dest= i.lower() + '.bmp'
+        dest= i.lower() + '.jpg'
     subprocess.call(['mv', tmp, dest])
