bc701999e728e260d3e530d0a157996382b3308b
vhb <vctrhb@gmail.com>
2014-11-26 21:22:55 +0100

    add generate.py

---
+++ data/generate.py
@@ -0,0 +1,13 @@
+import subprocess
+import shlex
+import json
+
+
+car_list = open('./car_list').read().split('\n')
+value = {}
+for i in car_list:
+    value[i] = []
+    cmd = shlex.split("find . -name {:s}.bmp".format(i))
+    files = subprocess.check_output(cmd).split('\n')
+    value[i] += [item for item in files if item]
+print(json.dumps(value))
